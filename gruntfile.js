7a5ea146a6d9ae9da6f2fe229eb44cc76e49f639
unknown <tpetrovic@ATGRZWS0015.exthex.local>
2015-09-09 11:40:59 +0200

    windows icons

--- gruntfile.js
+++ gruntfile.js
@@ -3,22 +3,21 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-            nwjs: {
-                options: {
-                    platforms: ['win'],
-                    buildDir: './build', // Where the build version of my node-webkit app is saved 
-                    version: 'v0.12.0'
-                },
-                src: ['./src/**/**', '!./src/app/lib/package.json', '!./src/config.*'] // Your node-webkit app 
-            }
+        nwjs: {
+            options: {
+                platforms: ['win'],
+                buildDir: './build', // Where the build version of my node-webkit app is saved 
+                version: 'v0.12.0',
+                winIco: './icons/ssgl.ico'
+            },
+            src: ['./src/**/**', '!./src/app/lib/package.json', '!./src/config.*'] // Your node-webkit app 
+        }    
     });
 
     // Plugins  
     
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-nw-builder');
-    
     grunt.registerTask('build', ['nwjs']);
-    
+
 
 };
