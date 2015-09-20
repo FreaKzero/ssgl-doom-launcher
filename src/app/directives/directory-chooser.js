cbca9f67d32065fbd05915fc153df312d564e780
FreaKzero <pete@freakzero.com>
2015-09-21 00:48:13 +0200

    file and directory picking dialogs

---
+++ src/app/directives/directory-chooser.js
@@ -0,0 +1,99 @@
+app.directive('dirChoose', function() {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        require: 'ngModel',
+        scope: {
+          dirpath: '=',
+          ngModel: '='     
+        },
+
+        template: '<div><md-input-container><label>{{label}}</label><input class="fileInput" type="text" ng-click="openDialog()" ng-model="ngModel"></md-input-container><input type="file" class="fileDialog" style="display:none" nwdirectory /></div>',
+        link: function($scope, elem, att, ngModel) {
+
+            $scope.label= att.label;
+
+            var z = elem[0].querySelector('.fileDialog');
+            var x = elem[0].querySelector('.fileInput');
+
+                z.addEventListener("change", function(evt) {
+                  
+                  if (this.value !== "") {
+                    var newPath = this.value + '\\';
+                    
+                    $scope.$apply(function() {
+                      $scope.ngModel = newPath;
+                    });
+
+                  }
+
+                }, false);
+    
+            $scope.openDialog = function() {
+              z.click();
+            };
+        }
+    };
+});
+
+app.directive('fileChoose', function() {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        require: 'ngModel',
+        scope: {
+          dirpath: '=',
+          ngModel: '='
+        },
+
+        template: '<div><md-input-container><label>{{label}}</label><input class="fileInput" type="text" ng-click="openDialog()" ng-model="ngModel"></md-input-container><input class="fileDialog" type="file" style="display:none;"></div>',        
+
+        link: function($scope, elem, att, ngModel) {
+
+            $scope.label= att.label;
+
+            var z = elem[0].querySelector('.fileDialog');
+            var x = elem[0].querySelector('.fileInput');
+
+                z.addEventListener("change", function(evt) {
+                  
+                  if (this.value !== "") {
+                    var newPath = this.value;
+                    
+                    $scope.$apply(function() {
+                      $scope.ngModel = newPath;
+                    });
+                    
+                  }
+
+                }, false);
+
+            
+            $scope.openDialog = function() {
+              z.click();
+            };
+        }
+    };
+});
+
+
+
+/*
+return {
+     restrict: 'A',
+     require: 'ngModel',
+     scope: {
+         ngModel: '='
+     },
+     link: function ($scope, $element, $attrs, ngModelCtrl) {
+
+        function updateView(value) {
+            ngModelCtrl.$viewValue = value;
+            ngModelCtrl.$render(); 
+        }
+
+        function updateModel(value) {
+            ngModelCtrl.$modelValue = value;
+            $scope.ngModel = value; // overwrites ngModel value
+        }
+*/
