7c4c7ea2ea2ecae061e61c7e85aa8f7f42c56f41
FreaKzero <pete@freakzero.com>
2015-10-04 22:20:42 +0200

    Linux builder

--- gruntfile.js
+++ gruntfile.js
@@ -1,51 +1,64 @@
-module.exports = function (grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    shell: {
-      npmInstall: {
-        command: 'cd src & npm install'
-      },
-      devInstall: {
-        command: 'rename nwjs-v0.12.0-win-x64 nw && copy cfg\\package.json nw\\package.json && cd nw & npm install'
-      }
-    },
+        shell: {
+            npmInstall: {
+                command: 'cd src & npm install'
+            },
+            devInstall: {
+                command: 'rename nwjs-v0.12.0-win-x64 nw && copy cfg\\package.json nw\\package.json && cd nw & npm install'
+            }
+        },
 
-    nwjs: {
-      options: {
-        platforms: ['win'],
-        buildDir: './build',
-        version: 'v0.12.0',
-        winIco: './icons/ssgl1.ico'
-      },
-      src: ['./src/**/**', '!./src/app/lib/package.json', '!./src/config.*']
-    },
+        nwjs: {
+            win: {
+                options: {
+                    platforms: ['win'],
+                    buildDir: './build',
+                    version: 'v0.12.0',
+                    winIco: './icons/ssgl1.ico'
+                },
+                src: ['./src/**/**', '!./src/app/lib/package.json', '!./src/config.*']
+            },
 
-    curl: {
+            tux: {
+                options: {
+                    platforms: ['linux'],
+                    buildDir: './build',
+                    version: 'v0.12.0'
+                },
+                src: ['./src/**/**', '!./src/app/lib/package.json', '!./src/config.*']
+            }
+        },
 
-      nwdev: {
-        src: 'http://dl.nwjs.io/v0.12.0/nwjs-v0.12.0-win-x64.zip',
-        dest: './cache/nwdev.zip'
-      }
-    },
+        curl: {
 
-    unzip: {
-      nwdev: {
-        src: './cache/nwdev.zip',
-        dest: './'
-      }
-    }
-  });
+            nwdev: {
+                src: 'http://dl.nwjs.io/v0.12.0/nwjs-v0.12.0-win-x64.zip',
+                dest: './cache/nwdev.zip'
+            }
+        },
 
-  // Plugins
+        unzip: {
+            nwdev: {
+                src: './cache/nwdev.zip',
+                dest: './'
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-nw-builder');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-curl');
-  grunt.loadNpmTasks('grunt-zip');
+    // Plugins
 
-  grunt.registerTask('build', ['nwjs']);
-  grunt.registerTask('init', ['shell:npmInstall']);
-  grunt.registerTask('build-devenv', ['curl', 'unzip', 'shell:devInstall']);
+    grunt.loadNpmTasks('grunt-nw-builder');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-curl');
+    grunt.loadNpmTasks('grunt-zip');
+
+    grunt.registerTask('build', ['nwjs:win']);
+    grunt.registerTask('build-linux', ['nwjs:tux']);
+    
+    grunt.registerTask('init', ['shell:npmInstall']);
+    grunt.registerTask('build-devenv', ['curl', 'unzip', 'shell:devInstall']);
 
 };
