43930d7fce3aa93d927dbcf0bb25e13a7ae5cf92
FreaKzero <pete@freakzero.com>
2016-02-03 00:07:27 +0100

    automating release process in gruntfile - not tested

--- gruntfile.js
+++ gruntfile.js
@@ -14,6 +14,18 @@ module.exports = function(grunt) {
             },
 			devInstallTux64: {
                 command: 'tar -xzvf cache/nwdev.tar.gz && mv nwjs-v0.12.0-linux-x64 nw && cp cfg/package.json nw/package.json && cd nw && npm install'
+            },
+
+            askPackage: {
+                command: 'cls && echo "Did you update your package.json and are on the Master Branch ?" && pause'
+            },
+
+            gitCommit: {
+                command: 'git commit -a -m "Release Version <%= pkg.version %>"'
+            },
+
+            gitTag: {
+                command: 'git tag v<%= pkg.version %>'
             }
         },
 
@@ -88,11 +100,18 @@ module.exports = function(grunt) {
     
     grunt.registerTask('build-win', ['nwjs:win']);
     grunt.registerTask('build-linux', ['nwjs:tux']);
-
     grunt.registerTask('init', ['shell:npmInstall']);
 
     grunt.registerTask('build-devenv-win', ['curl:win64', 'unzip', 'shell:devInstall']);
     grunt.registerTask('build-devenv-linux32', ['curl:nwdevTux32', 'shell:devInstallTux32']);
     grunt.registerTask('build-devenv-linux64', ['curl:nwdevTux64', 'shell:devInstallTux64']);
+    
+    grunt.registerTask('release', [
+        'shell:askPackage', 
+        'nwjs:win', 
+        'nwjs:tux', 
+        'shell:gitCommit', 
+        'shell:gitTag'
+    ]);    
 
 };
