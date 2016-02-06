757e6b3d7a99663b26006c5a5bac81ff886363da
FreaKzero <pete@freakzero.com>
2016-02-06 14:51:43 +0100

    redefined release process

--- gruntfile.js
+++ gruntfile.js
@@ -1,39 +1,69 @@
+/*
+[============================== AUTOMATIC RELEASE NOTES ==============================]
+|                                                                                     |
+| When starting grunt release dont update the version of the root package.json        |
+| This file is used for automatic updates.                                            |
+|                                                                                     |
+| Edit if after releasing - only update the src/package.json and start your release   |
+| process.                                                                            |
+|                                                                                     |
+| Grunt will open the necessary Websites for editing the root package.json and making |
+| your releasetag.                                                                    |
+|                                                                                     |
+[=====================================================================================]
+*/
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        
+        srcpkg: grunt.file.readJSON('src/package.json'),
 
         shell: {
             npmInstall: {
                 command: 'cd src && npm install'
             },
-            
+
             devInstall: {
                 command: 'rename nwjs-v0.12.0-win-x64 nw && copy cfg\\package.json nw\\package.json && cd nw & npm install'
             },
-            
-            devInstallTux32: {          
+
+            devInstallTux32: {
                 command: 'tar -xzvf cache/nwdev.tar.gz && mv nwjs-v0.12.0-linux-ia32 nw && cp cfg/package.json nw/package.json && cd nw && npm install'
             },
-            
+
             devInstallTux64: {
                 command: 'tar -xzvf cache/nwdev.tar.gz && mv nwjs-v0.12.0-linux-x64 nw && cp cfg/package.json nw/package.json && cd nw && npm install'
             },
 
             askPackage: {
-                command: 'echo "Did you update your package.json and are on the Master Branch ?" && pause'
+                command: 'echo "Did you update your src/package.json (v<%= srcpkg.version %>) and are on the Master Branch ?" && pause'
             },
 
             gitCommit: {
-                command: 'git commit -a -m "Release Version <%= pkg.version %>"'
+                command: 'git commit -a -m "Release Version <%= srcpkg.version %>"'
             },
 
             gitTag: {
-                command: 'git tag v<%= pkg.version %>'
+                command: 'git tag v<%= srcpkg.version %>'
             },
 
-            gitEnd: {
-                command: 'echo "SSGL Version v<%= pkg.version %> Built and Released !!! DONT FORGET TO PUSH !!!"'
-            }        
+            gitPush: {
+                command: 'echo "Press a Key for pushing to github" && pause && git push'
+            },
+
+            purgeBuilds: {
+                command: 'rm -rf build/SSGL && rm -rf build/RELEASE'
+            }
+        },
+
+        open : {
+            newRelease : {
+                path: 'https://github.com/FreaKzero/ssgl-doom-launcher/releases/new',
+            },
+
+            editPackage: {
+                path: 'https://github.com/FreaKzero/ssgl-doom-launcher/edit/master/package.json'
+            }
         },
 
         nwjs: {
@@ -81,13 +111,13 @@ module.exports = function(grunt) {
                 dest: './'
             }
         },
-        
+
         compress: {
             tux32: {
                 options: {
                     archive: './build/RELEASE/linux32.tar.gz',
                     mode: 'tgz'
-                },                
+                },
                 files: [{
                         src: ['**/*'],
                         expand: true,
@@ -99,7 +129,7 @@ module.exports = function(grunt) {
                 options: {
                     archive: './build/RELEASE/linux64.tar.gz',
                     mode: 'tgz'
-                },                
+                },
                 files: [{
                         src: ['**/*'],
                         expand: true,
@@ -110,7 +140,7 @@ module.exports = function(grunt) {
             win32: {
                 options: {
                     archive: './build/RELEASE/win32.zip'
-                },                
+                },
                 files: [{
                         src: ['**/*'],
                         expand: true,
@@ -121,13 +151,13 @@ module.exports = function(grunt) {
             win64: {
                 options: {
                     archive: './build/RELEASE/win64.zip'
-                },                
+                },
                 files: [{
                         src: ['**/*'],
                         expand: true,
                         cwd: './build/SSGL/win64/'
                     }]
-            }    
+            }
         },
 
         yuidoc: {
@@ -153,6 +183,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-zip');
     grunt.loadNpmTasks('grunt-contrib-yuidoc');
     grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-open');
 
     grunt.registerTask('build-win', ['nwjs:win']);
     grunt.registerTask('build-linux', ['nwjs:tux']);
@@ -161,15 +192,17 @@ module.exports = function(grunt) {
     grunt.registerTask('build-devenv-win', ['curl:win64', 'unzip', 'shell:devInstall']);
     grunt.registerTask('build-devenv-linux32', ['curl:nwdevTux32', 'shell:devInstallTux32']);
     grunt.registerTask('build-devenv-linux64', ['curl:nwdevTux64', 'shell:devInstallTux64']);
-    
+
     grunt.registerTask('release', [
-        'shell:askPackage', 
-        'nwjs:win', 
-        'nwjs:tux', 
+        'shell:askPackage',
+        'shell:purgeBuilds',
+        'nwjs',
         'compress',
-        'shell:gitCommit', 
-        'shell:gitTag',        
-        'shell:gitEnd'
-    ]);    
+        'shell:gitCommit',
+        'shell:gitTag',
+        'shell:gitPush',
+        'open:editPackage',
+        'open:newRelease'
+    ]);
 
 };
