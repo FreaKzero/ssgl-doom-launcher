517f076c70063f36968de5d39679e00bd2183530
FreaKzero <pete@freakzero.com>
2016-09-24 09:05:32 +0200

    Fix registerMenu

--- src/app/main.js
+++ src/app/main.js
@@ -64,7 +64,7 @@ var app = angular.module('ssgl', ['ngMaterial', 'ui.router']);
         $rootScope.APPVERSION = nwService.readSyncJSON('package.json').version;
         document.title = 'Super Shotgun Launcher v' + $rootScope.APPVERSION;
         nwService.mkDir(nwService.buildPath(['lists'], true), true);
-        nwService.registerMenu();
+        
 
         if (nwService.hasArg('-r') || nwService.hasArg('--livereload')) {
             try {
