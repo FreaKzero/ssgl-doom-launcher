677ee2b760b0122ceac6b122e1ad459ccc65e9f9
FreaKzero <pete@freakzero.com>
2016-10-11 01:36:14 +0200

    Add ESLINT

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+
+    "globals": {
+        "app": true,
+        "angular": true
+    },
+
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
