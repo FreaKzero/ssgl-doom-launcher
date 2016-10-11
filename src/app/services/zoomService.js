19c040ae5e6defa3d2c853be6db7e29428496883
FreaKzero <pete@freakzero.com>
2016-10-11 20:46:48 +0200

    Fix ESLint

--- src/app/services/zoomService.js
+++ src/app/services/zoomService.js
@@ -1,36 +1,36 @@
 (function() {
-    app.factory('zoomService', ['nwService', 'hotkeys', zoomService]);
+  app.factory('zoomService', ['nwService', 'hotkeys', zoomService]);
 
-    function zoomService(nwService, hotkeys) {
-        var service = {};
-        var Window = nwService.getWindow();
+  function zoomService(nwService, hotkeys) {
+    var service = {};
+    var Window = nwService.getWindow();
 
-        var C = {
-            MAXZOOM : 5,
-            MINZOOM: 0,
-            STEP: 0.5
-        };
+    var C = {
+      MAXZOOM : 5,
+      MINZOOM: 0,
+      STEP: 0.5
+    };
 
-        hotkeys.add({
-            combo: 'ctrl+=',
-            description: 'Zoom In',
-            callback: function() {
-                if (Window.zoomLevel <= C.MAXZOOM) {
-                    Window.zoomLevel += C.STEP;
-                }
-            }
-        });
+    hotkeys.add({
+      combo: 'ctrl+=',
+      description: 'Zoom In',
+      callback: function() {
+        if (Window.zoomLevel <= C.MAXZOOM) {
+          Window.zoomLevel += C.STEP;
+        }
+      }
+    });
 
-        hotkeys.add({
-            combo: 'ctrl+-',
-            description: 'Zoom Out',
-            callback: function() {
-                if (Window.zoomLevel >= C.MINZOOM) {
-                    Window.zoomLevel -= C.STEP;
-                }
-            }
-        });
+    hotkeys.add({
+      combo: 'ctrl+-',
+      description: 'Zoom Out',
+      callback: function() {
+        if (Window.zoomLevel >= C.MINZOOM) {
+          Window.zoomLevel -= C.STEP;
+        }
+      }
+    });
 
-        return service;
-    }
+    return service;
+  }
 })();
