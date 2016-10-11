54f379fc3955476221dd38ea75d34d2550fc3cd9
FreaKzero <pete@freakzero.com>
2016-10-11 03:37:08 +0200

    Add updateService

---
+++ src/app/services/updateService.js
@@ -0,0 +1,9 @@
+(function() {
+  app.factory('updateService', [updateService]);
+
+  function updateService() {
+    var service = {};   
+    //newurl: 'https://api.github.com/repos/FreaKzero/ssgl-doom-launcher/releases'
+    return service;
+  }
+})();
