a64d078d2a94a17731f7230c2a6271d14d063b06
FreaKzero <pete@freakzero.com>
2016-10-11 21:10:40 +0200

    Fix ESLint

--- src/app/directives/directory-chooser.js
+++ src/app/directives/directory-chooser.js
@@ -1,53 +1,53 @@
 (function() {
-    app.directive('dirChoose', ['nwService', function(nwService) {
-        return {
-            restrict: 'AE',
-            replace: 'true',
-            require: 'ngModel',
-            scope: {
-                dirpath: '=?',
-                ngModel: '=?',
-                wdir: '=?'
-            },
+  app.directive('dirChoose', ['nwService', function(nwService) {
+    return {
+      restrict: 'AE',
+      replace: 'true',
+      require: 'ngModel',
+      scope: {
+        dirpath: '=?',
+        ngModel: '=?',
+        wdir: '=?'
+      },
 
-            template: '<div layout="row"><div flex="90"><md-input-container><label>{{label}}</label><input class="fileInput" type="text" ng-model="ngModel"></md-input-container></div><div flex="10"><md-button class="fileBtn md-accent" ng-click="openDialog()"><i class="mdi mdi-folder"></i> Folder</md-button><input type="file" nwworkingdir="{{wdir}}" class="fileDialog" style="display:none" nwdirectory /></div></div>',
-            link: function($scope, elem, att, ngModel) {
+      template: '<div layout="row"><div flex="90"><md-input-container><label>{{label}}</label><input class="fileInput" type="text" ng-model="ngModel"></md-input-container></div><div flex="10"><md-button class="fileBtn md-accent" ng-click="openDialog()"><i class="mdi mdi-folder"></i> Folder</md-button><input type="file" nwworkingdir="{{wdir}}" class="fileDialog" style="display:none" nwdirectory /></div></div>',
+      link: function($scope, elem, att, ngModel) {
 
-                if (typeof $scope.ngModel !== 'undefined' && $scope.ngModel !== '') {
-                    $scope.wdir = $scope.ngModel;                    
-                } else {
-                    $scope.wdir = nwService.execpath;
-                }
+        if (typeof $scope.ngModel !== 'undefined' && $scope.ngModel !== '') {
+          $scope.wdir = $scope.ngModel;                    
+        } else {
+          $scope.wdir = nwService.execpath;
+        }
                 
-                $scope.label = att.label;
+        $scope.label = att.label;
 
-                var z = elem[0].querySelector('.fileDialog');
-                var x = elem[0].querySelector('.fileInput');
+        var z = elem[0].querySelector('.fileDialog');
+        var x = elem[0].querySelector('.fileInput');
 
-                x.addEventListener('blur', function(evt) {
-                    if (x.value.slice(-1) !== nwService.pathsep) {                        
-                        $scope.$apply(function() {
-                            x.value += nwService.pathsep;
-                            $scope.ngModel = x.value;
-                        });                                            
-                    }
-                });
+        x.addEventListener('blur', function(evt) {
+          if (x.value.slice(-1) !== nwService.pathsep) {                        
+            $scope.$apply(function() {
+              x.value += nwService.pathsep;
+              $scope.ngModel = x.value;
+            });                                            
+          }
+        });
 
-                z.addEventListener('change', function(evt) {
-                    if (this.value !== '') {
-                        var newPath = this.value + nwService.pathsep;
+        z.addEventListener('change', function() {
+          if (this.value !== '') {
+            var newPath = this.value + nwService.pathsep;
 
-                        $scope.$apply(function() {
-                            $scope.ngModel = newPath;
-                        });
-                    }
+            $scope.$apply(function() {
+              $scope.ngModel = newPath;
+            });
+          }
 
-                }, false);
+        }, false);
 
-                $scope.openDialog = function() {
-                    z.click();
-                };
-            }
+        $scope.openDialog = function() {
+          z.click();
         };
-    }]);
+      }
+    };
+  }]);
 })();
--- src/app/directives/file-chooser.js
+++ src/app/directives/file-chooser.js
@@ -1,46 +1,44 @@
 (function() {
-    app.directive('fileChoose', ['nwService', function(nwService) {
-        return {
-            restrict: 'AE',
-            replace: 'true',
-            require: 'ngModel',
-            scope: {
-                dirpath: '=?',
-                ngModel: '=?',
-                wdir: '=?'
-            },
+  app.directive('fileChoose', ['nwService', function(nwService) {
+    return {
+      restrict: 'AE',
+      replace: 'true',
+      require: 'ngModel',
+      scope: {
+        dirpath: '=?',
+        ngModel: '=?',
+        wdir: '=?'
+      },
 
-            template: '<div layout="row"><div flex="90"><md-input-container><label>{{label}}</label><input class="fileInput" type="text" ng-model="ngModel"></md-input-container></div><div flex="10"><md-button class="fileBtn md-accent" ng-click="openDialog()"><i class="mdi mdi-open-in-app"></i> File</md-button><input class="fileDialog" nwworkingdir="{{wdir}}" type="file" style="display:none;"></div></div>',
+      template: '<div layout="row"><div flex="90"><md-input-container><label>{{label}}</label><input class="fileInput" type="text" ng-model="ngModel"></md-input-container></div><div flex="10"><md-button class="fileBtn md-accent" ng-click="openDialog()"><i class="mdi mdi-open-in-app"></i> File</md-button><input class="fileDialog" nwworkingdir="{{wdir}}" type="file" style="display:none;"></div></div>',
 
-            link: function($scope, elem, att, ngModel) {
+      link: function($scope, elem, att, ngModel) {
 
-                if (typeof $scope.ngModel !== 'undefined' && $scope.ngModel !== '') {
-                    $scope.wdir = $scope.ngModel.substring(0, $scope.ngModel.lastIndexOf(nwService.pathsep));
-                } else {
-                    $scope.wdir = nwService.execpath;
-                }
+        if (typeof $scope.ngModel !== 'undefined' && $scope.ngModel !== '') {
+          $scope.wdir = $scope.ngModel.substring(0, $scope.ngModel.lastIndexOf(nwService.pathsep));
+        } else {
+          $scope.wdir = nwService.execpath;
+        }
 
-                $scope.label = att.label;
+        $scope.label = att.label;
 
-                var z = elem[0].querySelector('.fileDialog');
-                var x = elem[0].querySelector('.fileInput');
+        var z = elem[0].querySelector('.fileDialog'); 
+        z.addEventListener('change', function() {
 
-                z.addEventListener("change", function(evt) {
+          if (this.value !== '') {
+            var newPath = this.value;
 
-                    if (this.value !== "") {
-                        var newPath = this.value;
+            $scope.$apply(function() {
+              $scope.ngModel = newPath;
+            });
+          }
 
-                        $scope.$apply(function() {
-                            $scope.ngModel = newPath;
-                        });
-                    }
+        }, false);
 
-                }, false);
-
-                $scope.openDialog = function() {
-                    z.click();
-                };
-            }
+        $scope.openDialog = function() {
+          z.click();
         };
-    }]);
+      }
+    };
+  }]);
 })();
