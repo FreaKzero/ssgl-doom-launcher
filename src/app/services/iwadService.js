7961c05f5ad6a2bb9c4ea9a7fec1b87bce1370f0
FreaKzero <pete@freakzero.com>
2016-10-11 20:47:13 +0200

    Fix ESlint

--- src/app/services/iwadService.js
+++ src/app/services/iwadService.js
@@ -1,6 +1,6 @@
 (function() {
 
-    app.factory('iwadService', ['$q', 'nwService', iwadService]);
+  app.factory('iwadService', ['$q', 'nwService', iwadService]);
 
      /**
      * Service to Fetch iWads from the Filesystem
@@ -10,7 +10,7 @@
      * @submodule iwadService
      * @uses nwService
      */
-    function iwadService($q, nwService) {
+  function iwadService($q, nwService) {
 
         /**
          * Filenames needed for Covers
@@ -19,7 +19,7 @@
          * @private
          * @type {Array}
          */
-        var covers = ['chex', 'chex2', 'doom', 'doom2', 'doom64', 'freedm', 'freedoom1', 'freedoom2', 'hacx', 'heretic', 'heretic1', 'hexdd', 'hexen', 'plutonia', 'strife0', 'strife1', 'tnt'];
+    var covers = ['chex', 'chex2', 'doom', 'doom2', 'doom64', 'freedm', 'freedoom1', 'freedoom2', 'hacx', 'heretic', 'heretic1', 'hexdd', 'hexen', 'plutonia', 'strife0', 'strife1', 'tnt'];
         /**
          * When no cover is found use this Image
          * 
@@ -27,16 +27,16 @@
          * @private
          * @type {String}
          */
-        var nocover = 'app/assets/ssgl.png';     
+    var nocover = 'app/assets/ssgl.png';     
 
-        return {
+    return {
             /**
              * The iWads
              * 
              * @property IWADS             
              * @type {Array}
              */
-            IWADS: [],
+      IWADS: [],
 
             /**
              * Wrapper for getting Iwads from Filesystem
@@ -46,13 +46,13 @@
              * @param  {String} path
              * @return {Array}  iwads
              */
-            getIWADS: function(path) {
-                if (this.IWADS.length === 0) {
-                    this.IWADS = this.read(path);
-                }
+      getIWADS: function(path) {
+        if (this.IWADS.length === 0) {
+          this.IWADS = this.read(path);
+        }
 
-                return this.IWADS;
-            },
+        return this.IWADS;
+      },
 
             /**
              * Reads iwads from the Filesystem and mutate Data, async
@@ -62,33 +62,33 @@
              * @param  {String} path
              * @return {Promise}
              */
-            read: function(path) {
-                var def = $q.defer();
+      read: function(path) {
+        var def = $q.defer();
                 
-                nwService.getDir(path).then(function(files) {
-                    var cover = nocover;
+        nwService.getDir(path).then(function(files) {
+          var cover = nocover;
 
-                    var iwads = files.map(function(item) {
-                        var n = item.split('.')[0].toLowerCase();
+          var iwads = files.map(function(item) {
+            var n = item.split('.')[0].toLowerCase();
 
-                        if (covers.indexOf(n) > -1) {
-                            cover = 'app/assets/covers/' + n + '.jpg';
-                        }
-
-                        return {
-                            file: item,
-                            name: n.toUpperCase(),
-                            cover: cover
-                        };
-                    });
-
-                    def.resolve(iwads);
-                    
-                });
-
-                return def.promise;
+            if (covers.indexOf(n) > -1) {
+              cover = 'app/assets/covers/' + n + '.jpg';
             }
 
-        };
-    }
+            return {
+              file: item,
+              name: n.toUpperCase(),
+              cover: cover
+            };
+          });
+
+          def.resolve(iwads);
+                    
+        });
+
+        return def.promise;
+      }
+
+    };
+  }
 })();
