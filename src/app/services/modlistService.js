957439f2c17905b1657424ed3f68759287155343
FreaKzero <pete@freakzero.com>
2016-10-16 09:35:03 +0200

    Fix indents of comments

--- src/app/services/modlistService.js
+++ src/app/services/modlistService.js
@@ -1,34 +1,34 @@
 (function() {
   app.factory('modlistService', ['$q', '$rootScope', 'nwService', modlistService]);
 
-     /**
-     * CRUD for Modlists (sidebar)
-     *
-     * @method modlistService
-     * @module ssgl
-     * @submodule modlistService
-     */
+   /**
+   * CRUD for Modlists (sidebar)
+   *
+   * @method modlistService
+   * @module ssgl
+   * @submodule modlistService
+   */
   function modlistService($q, $rootScope, nwService) {
     var service = {};
-        /**
-         * Directory where the Lists are
-         *
-         * @property listDir
-         * @private
-         * @type {String}
-         */
+    /**
+     * Directory where the Lists are
+     *
+     * @property listDir
+     * @private
+     * @type {String}
+     */
     var listDir = $rootScope.config.modlistpath; 
 
     service.lists = [];
-        /**
-         * Reame a List async
-         *
-         * @method rename
-         * @async
-         * @for modlistService
-         * @param  {Object} item
-         * @return {Promise}
-         */
+    /**
+     * Reame a List async
+     *
+     * @method rename
+     * @async
+     * @for modlistService
+     * @param  {Object} item
+     * @return {Promise}
+     */
     service.rename = function(item) {
       var oldPath = item.path;
       var newPath = nwService.buildPath([listDir, item.name + '.json']);
@@ -38,28 +38,28 @@
       return nwService.rename(oldPath, newPath);
     };
 
-        /**
-         * Removes Item async
-         *
-         * @method remove
-         * @async
-         * @for modlistService
-         * @param  {Object} item
-         * @return {Promise}
-         */
+    /**
+     * Removes Item async
+     *
+     * @method remove
+     * @async
+     * @for modlistService
+     * @param  {Object} item
+     * @return {Promise}
+     */
     service.remove = function(item) {
       return nwService.remove(item.path);
     };
 
-        /**
-         * Saves selected lists into JSON File async
-         *
-         * @method saveSelected
-         * @for modlistService
-         * @param  {Object} listObj {name: '', list: ''}
-         * @async
-         * @return {Promise}
-         */
+    /**
+     * Saves selected lists into JSON File async
+     *
+     * @method saveSelected
+     * @for modlistService
+     * @param  {Object} listObj {name: '', list: ''}
+     * @async
+     * @return {Promise}
+     */
     service.saveSelected = function(listObj) {
       var existingIndex = _.findIndex(service.lists, function(item) {
         return listObj.name === item.name;
@@ -85,14 +85,14 @@
       );
     };
 
-        /**
-         * Fetching Lists from Directory async
-         *
-         * @method getLists
-         * @for modlistService
-         * @async
-         * @return {Promise}
-         */
+  /**
+   * Fetching Lists from Directory async
+   *
+   * @method getLists
+   * @for modlistService
+   * @async
+   * @return {Promise}
+   */
     service.getLists = function() {
       var def = $q.defer();
             
