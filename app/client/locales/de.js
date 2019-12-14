482a02e2d3c5159f7322bbd9bcbaf14bfa463643
FreaKzero <pete@freakzero.com>
2019-12-14 15:01:49 +0100

    German Translation

---
+++ app/client/locales/de.js
@@ -0,0 +1,25 @@
+export default {
+  atoms: {
+    reset: 'Reset',
+    selectFile: 'Datei',
+    selectDir: 'Ordner'
+  },
+  wads: {
+    filter: 'Filter'
+  },
+  settings: {
+    waddir: 'Wad Verzeichnis',
+    wallpaper: 'Hintergrundbild',
+    save: 'Einstellungen Speichern'
+  },
+  common: {
+    loading: 'Laden'
+  },
+  nav: {
+    appname: 'Super Shotgun Launcher',
+    wads: 'Wads',
+    packages: 'Pakete',
+    sourceports: 'Sourceports',
+    settings: 'Einstellungen'
+  }
+};
--- app/client/locales/index.js
+++ app/client/locales/index.js
@@ -1,4 +1,12 @@
 import en from './en';
+import de from './de';
+
+export const AVAILABLE_LOCALES = {
+  en: 'English',
+  de: 'Deutsch'
+};
+
 export default {
-  en
+  en,
+  de
 };
