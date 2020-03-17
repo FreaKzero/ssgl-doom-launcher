f8c4b28c73b0f0a5354a1781ff694fd51b3746a3
FreaKzero <pete@freakzero.com>
2020-03-17 18:19:40 +0100

    snapcraft build and release

---
+++ release-app.sh
@@ -0,0 +1,22 @@
+git checkout latest
+git pull
+git checkout master
+git pull
+git merge latest
+
+VERSION="v"
+VERSION+=$(jq -r .version ./app/package.json)
+
+echo "Check if Tag $VERSION exists"
+if GIT_DIR=.git git rev-parse $VERSION >/dev/null 2>&1
+then
+# Found
+    echo "Version $VERSION already exists - please change it in the package.json and dont commit the change"
+else
+# Not Found
+    git commit -am "$VERSION"
+    git tag "$VERSION"
+    git push && git push --tags
+    git checkout latest
+    open "https://github.com/FreaKzero/ssgl-doom-launcher/actions"
+fi
