e523a0a20bf5c4c622b1bb3995d6d7f8d2054139
FreaKzero <pete@freakzero.com>
2020-03-19 02:33:23 +0100

    FEAT: -bex support

---
+++ app/electron/utils/play.js
@@ -0,0 +1,97 @@
+import { spawn } from 'child_process';
+import { existsSync, readdirSync, statSync } from 'fs';
+import { platform } from 'os';
+import { join } from 'path';
+
+import { copyfile, createPath } from './common';
+
+const getLastSaveGame = dir => {
+  if (!existsSync(dir)) {
+    return null;
+  }
+
+  // const latestSave = getLastSaveGame(`${BASEDIR}/saves`);
+  // COMMAND = COMMAND.concat(['-loadgame', latestSave]);
+
+  const files = readdirSync(dir);
+  return files
+    .map(name => {
+      const p = join(dir, name);
+      return { path: p, ctime: statSync(p).ctime };
+    })
+    .sort((a, b) => b.ctime - a.ctime)[0].path;
+};
+
+const play = (pack, settings) => {
+  let deh = [];
+  let bex = [];
+  let file = [];
+  let params = [];
+
+  const { iwad, selected, sourceport, name } = pack;
+
+  const BASEDIR = `${settings.savepath}/${sourceport.name}/${name}`;
+
+  if (settings.savepath.trim() !== '' && !existsSync(BASEDIR)) {
+    createPath(BASEDIR);
+    if (existsSync(sourceport.configDefault) && sourceport.hasConfig) {
+      copyfile(
+        sourceport.configDefault,
+        `${BASEDIR}/${sourceport.configFilename}`
+      );
+    }
+  }
+
+  selected.forEach(i => {
+    switch (i.kind) {
+      case 'DEH':
+        deh.push(i.path);
+        break;
+      case 'BEX':
+        bex.push(i.path);
+        break;
+      default:
+        file.push(i.path);
+    }
+  });
+
+  if (sourceport.hasConfig) {
+    params = params.concat([
+      sourceport.paramConfig,
+      `${BASEDIR}/${sourceport.configFilename}`
+    ]);
+  }
+
+  if (sourceport.hasSavedir) {
+    params = params.concat([sourceport.paramSave, `${BASEDIR}/saves`]);
+  }
+
+  let COMMAND = ['-iwad', iwad.path, '-file', ...file];
+
+  if (params.length > 0) {
+    COMMAND = COMMAND.concat(params);
+  }
+
+  if (deh.length > 0) {
+    COMMAND = COMMAND.concat(['-deh', ...deh]);
+  }
+
+  if (bex.length > 0) {
+    COMMAND = COMMAND.concat(['-bex', ...bex]);
+  }
+
+  if (platform() === 'darwin') {
+    let MAC = [sourceport.binary, '--args'];
+    COMMAND = [...MAC, ...COMMAND];
+    spawn('open', COMMAND);
+  } else {
+    spawn(sourceport.binary, COMMAND);
+  }
+
+  return {
+    data: pack,
+    error: null
+  };
+};
+
+export default play;
