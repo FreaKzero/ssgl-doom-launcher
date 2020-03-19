8217b27b124894ad542009042e57cf2000ac14f9
FreaKzero <pete@freakzero.com>
2020-03-19 13:34:49 +0100

    Changelogscript for Releasing

--- release-app.sh
+++ release-app.sh
@@ -1,8 +1,11 @@
+#!/bin/sh
 git checkout master
 git merge latest
 
 VERSION="v"
 VERSION+=$(jq -r .version ./app/package.json)
+
+LASTTAG=$(git describe --tags --abbrev=0)
 
 echo "Check if Tag $VERSION exists"
 if GIT_DIR=.git git rev-parse $VERSION >/dev/null 2>&1
@@ -16,5 +19,13 @@ else
     git push && git push --tags
     git checkout latest
     git merge master
+
+    echo "## Changelog:"
+    if [ -z "$1" ]; then
+      echo $(git log --pretty=format:%s "$LASTTAG"..HEAD | grep -E "FIX|ADD|FEATURE|BREAK|REFACTOR" | awk '{print "\\n- "$0}')
+    else 
+      echo $(git log --pretty=format:%s "$LASTTAG"..HEAD | grep -E "FIX|ADD|FEATURE|BREAK|REFACTOR" | awk '{print "\\n- "$0}')
+    fi
+
     open "https://github.com/FreaKzero/ssgl-doom-launcher/actions"
 fi
