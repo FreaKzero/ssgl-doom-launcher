12c9e9b8cadf55812aa7b71eea5889b3406aef72
FreaKzero <pete@freakzero.com>
2020-03-21 10:19:52 +0100

    Custom Menu

--- app/electron/menu.js
+++ app/electron/menu.js
@@ -1,18 +1,79 @@
 const { app, Menu } = require('electron');
+const { shell } = require('electron');
+const isMac = process.platform === 'darwin';
 
-const menu = Menu.buildFromTemplate([
-  {
-    label: 'Menu',
-    submenu: [
-      { type: 'separator' },
-      {
-        label: 'Exit',
-        click() {
-          app.quit();
+
+// {
+//   label: 'Join Discord',
+//   click: async () => {
+//     await shell.openExternal('https://discord.gg/MsjZhHF');
+//   }
+// },
+// {
+//   label: 'Open Github',
+//   click: async () => {
+//     await shell.openExternal('https://github.com/FreaKzero/ssgl-doom-launcher');
+//   }
+// },
+
+const template = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideothers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+          ]
         }
-      }
+      ]
+    : []),
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac
+        ? [{ role: 'delete' }, { role: 'selectAll' }, { type: 'separator' }]
+        : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }])
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac
+        ? [{ type: 'separator' }, { role: 'front' }]
+        : [{ role: 'close' }])
     ]
   }
-]);
+];
+
+const menu = Menu.buildFromTemplate(template);
 
 module.exports = menu;
