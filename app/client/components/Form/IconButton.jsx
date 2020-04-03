93f714334bffeb649b50cc6cb274680a9d1a0b39
FreaKzero <pete@freakzero.com>
2020-04-03 07:48:03 +0200

    Adding Tooltip possibility for IconButtons

--- app/client/components/Form/IconButton.jsx
+++ app/client/components/Form/IconButton.jsx
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 import { ButtonStyle } from './Button';
 
 const IconButtonStyle = styled(ButtonStyle)`
+  position: relative;
   min-width: 0;
   height: 37px;
   width: 47px;
@@ -29,11 +30,47 @@ const IconButtonStyle = styled(ButtonStyle)`
     filter: ${({ theme }) => theme.svg.glow};
     fill: ${({ theme }) => theme.color.active};
   }
+
+  @keyframes anim-tooltip {
+    0% {
+      opacity: 0;
+      transform: translateY(15px);
+    }
+
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
+  .tooltip {
+    position: absolute;
+    font-size: 13px;
+    bottom: 110%;
+    left: 50%;
+    margin-left: -60px;
+    visibility: hidden;
+    width: 120px;
+    background-color: black;
+    color: #fff;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+    z-index: 101;
+  }
+
+  &:hover .tooltip {
+    animation-name: anim-tooltip;
+    animation-duration: 0.45s;
+    animation-timing-function: ease-out;
+    visibility: visible;
+  }
 `;
 
-const IconButton = ({ svg, load, ...rest }) => (
+const IconButton = ({ svg, load, tooltip, ...rest }) => (
   <>
     <IconButtonStyle className={load ? 'load' : undefined} {...rest}>
+      {tooltip ? <div className="tooltip">{tooltip}</div> : null}
       <SvgInline svg={svg} />
     </IconButtonStyle>
   </>
@@ -41,7 +78,8 @@ const IconButton = ({ svg, load, ...rest }) => (
 
 IconButton.propTypes = {
   load: PropTypes.bool,
-  svg: PropTypes.any.isRequired
+  svg: PropTypes.any.isRequired,
+  tooltip: PropTypes.string
 };
 
 export default IconButton;
