9df58f69a3212709a1b6b7476ec84a398cf90dc1
FreaKzero <pete@freakzero.com>
2020-04-11 15:19:27 +0200

    Nicer Update Notifier

--- app/client/components/Update.jsx
+++ app/client/components/Update.jsx
@@ -1,10 +1,40 @@
 import { remote } from 'electron';
 import React, { useContext } from 'react';
+import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components';
 
 import { StoreContext } from '../state';
 import { Button } from './Form';
 import { Modal } from './index';
 
+const MarkdownStyle = styled.div`
+  max-height: 350px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  background-color: black;
+  border-radius: 4px;
+  padding: 0 10px 10px 10px;
+  ${({ theme }) => theme.scrollbar};
+
+  p {
+    margin-bottom: 10px;
+  }
+
+  ul {
+    color: red;
+    list-style-type: square;
+    padding-left: 20px;
+    margin: 10px;
+  }
+
+  code {
+    display: block;
+    padding: 10px;
+    border-radius: 4px;
+    border: 1px solid darkgrey;
+    margin: 10px 0 10px 0;
+  }
+`;
 const Update = () => {
   const { gstate, dispatch } = useContext(StoreContext);
 
@@ -23,7 +53,9 @@ const Update = () => {
       title={`Version ${gstate.update.version} Available`}
       strict
     >
-      <pre>{gstate.update.changelog}</pre>
+      <MarkdownStyle>
+        <ReactMarkdown source={gstate.update.changelog} />
+      </MarkdownStyle>
       <div style={{ textAlign: 'right', marginTop: '20px' }}>
         <Button
           type="button"
